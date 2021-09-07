2f7db4173ff78c27965f7cddce7680c53951bb03
abhim42 <abhilashamishra777@gmail.com>
2021-09-07 13:35:11 +0530

    add whatsapp link

--- js/scripts.js
+++ js/scripts.js
@@ -117,17 +117,17 @@ var swiper = new Swiper(".colorPencilSwiper", {
 // setImg();
 
 // With jQuery
-$(document).on({
-  contextmenu: function (e) {
-    console.log("ctx menu button:", e.which);
+// $(document).on({
+//   contextmenu: function (e) {
+//     console.log("ctx menu button:", e.which);
 
-    // Stop the context menu
-    e.preventDefault();
-  },
-  mousedown: function (e) {
-    console.log("normal mouse down:", e.which);
-  },
-  mouseup: function (e) {
-    console.log("normal mouse up:", e.which);
-  },
-});
+//     // Stop the context menu
+//     e.preventDefault();
+//   },
+//   mousedown: function (e) {
+//     console.log("normal mouse down:", e.which);
+//   },
+//   mouseup: function (e) {
+//     console.log("normal mouse up:", e.which);
+//   },
+// });
