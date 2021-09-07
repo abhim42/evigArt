8aff23032ce336827355e652da85ad585920ef14
abhim42 <abhilashamishra777@gmail.com>
2021-09-07 10:59:55 +0530

    disable right click option

--- js/scripts.js
+++ js/scripts.js
@@ -115,3 +115,19 @@ var swiper = new Swiper(".colorPencilSwiper", {
 // };
 
 // setImg();
+
+// With jQuery
+$(document).on({
+  contextmenu: function (e) {
+    console.log("ctx menu button:", e.which);
+
+    // Stop the context menu
+    e.preventDefault();
+  },
+  mousedown: function (e) {
+    console.log("normal mouse down:", e.which);
+  },
+  mouseup: function (e) {
+    console.log("normal mouse up:", e.which);
+  },
+});
